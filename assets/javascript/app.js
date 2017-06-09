6778b3a084c197ee9518ab396f6892528fe8ba22
Cody Chase <Cody Chase>
2017-06-08 20:46:21 -0400

    started search button logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+//search button
+$("#submit").on("click", function(){
+    var searchTerm = $("#search").val().trim();
+    var numRecords = $("#numberRecords").val().trim();
+    var yearStart = $("#startYear").val().trim();
+    var yearEnd = $("#endYear").val().trim();
+    
+    for(var i = 0; i < numRecords; i++){
+        
+    }
+});
